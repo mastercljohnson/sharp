f0e8c640c9b02716bacc91c27943ff9614bbaf2a
mastercljohnson <mastercljohnson@gmail.com>
2018-06-29 00:21:31 -0700

    Made Comments and cleaned up code

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sharp
-python code for summarizing CT dosage data from csv files
+Python code for summarizing CT dosage data from csv files
+
+appface1.py
+-------------
+ Used for file handling
+
+pediatric_general.py
+----------------------
+Creates excel files containing basic statistics of test data in the csv files
+specific to institution and machine
--- pediatric_general.py
+++ pediatric_general.py
@@ -1,24 +1,19 @@
+# This is a script which takes in csv files of radiation dosage from specific
+# examination and outputs basic statistics about these tests specific to each
+# machine and each hospital
+
+# Import libraries for manipulating data tables
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
-
-    #import tkinter as tk
-    #from tkinter import filedialog
 import scipy
 from scipy import stats
-
 import xlsxwriter
 
 
 class pediatric_general:
 
-   
-
-#root = tk.Tk()
-#root.withdraw()
-#file_path = filedialog.askopenfilename()
-
-
+    # Initialize tables for different types of radiation values
     def __init__(self, file_path):
         self.maniparraydlp = np.array([])
         self.maniparrayctdi = np.array([])
@@ -31,72 +26,58 @@ class pediatric_general:
         self.minarrayctdi = np.array([])
         self.file_path = file_path
 
-    
-
-        
-
-
-    
+    # Load csv files data chosen from path into pandas table
     def sort(self):
-        
-
-
-
         data = pd.read_csv(self.file_path, names =["MRN","A#","date","time",
-                                          "proced","prot_name","prot","Institution","equip","device",
+                                          "proced","prot_name","prot"
+                                          ,"Institution","equip","device",
                                           "CTDI_vol","dlp","103_ICRP"
                                           ])
-#print(data["Institution"])
-        #inst = data["Institution"]
-        #["SGH","SMH","SCVMC","SRS","SCO","SCVIC", "SGH GMT", "SMH OPP"]
-        
+
+        # Parse the values which contain 0 and are repeated
         inst = np.unique(data["Institution"].values)
         inst = np.delete(inst, 0)
-        print(inst)
-#dev = ["Aquilion","BrightSpeed","LightSpeed Ultra","LightSpeed VCT", "LightSpeed16"]
         equip = np.unique(data["equip"].values)
 
-        
+        # Create a loop through the datatable which matches
+        # institutions with their equipment
+        # and outputs basic statistics for those equipment
 
-
-        a=0
-
-
+        # Checks for each Institution
         for n in inst:
             nn = str(n)
             data_inst = data[data["Institution"].str.contains(nn)]
+            # Match equipment inside institutions
             for p in equip:
                 pp = str(p)
                 data_equip = data_inst[data_inst["equip"].str.contains(pp)]
 
                 DLP_ar = data_equip["dlp"]
                 CTDI_ar = data_equip["CTDI_vol"]
-        
 
-
-        
-
-        
-    #r=pd.DataFrame(data_inst,columns = ["MRN", "DOB", "Gender", "Weight", "Height", "A#","Description", "Date_Performed", "Time_Performed",
-                                      #"Institution","equipment","device","Scan_Region","Images",
-                                      #"does_sheet","eye_ct","eye_total","103_ICRP",
-                                      #"ov_ct","ov_nm","ov_tot","remainderircp","test_tot", "ut_ct",
-                                      #"ut_tot"])
+                # Checks if the institution runs the specified test
+                # Creates an empty file if nothing was found
                 if data_equip.empty is False:
-                #print('m')
-                #print(descriptarray)
-                #print(DLP_ar.values)
-                    self.minarraydlp = np.append( self.minarraydlp , np.nanmin(DLP_ar.dropna().values))
-                    self.minarrayctdi = np.append( self.minarrayctdi , np.nanmin(CTDI_ar.dropna().values))
-                    self.maxarraydlp = np.append( self.maxarraydlp , np.nanmax(DLP_ar.dropna().values))
-                    self.maxarrayctdi = np.append( self.maxarrayctdi , np.nanmax(CTDI_ar.dropna().values))
+                    self.minarraydlp = np.append( self.minarraydlp
+                                            , np.nanmin(DLP_ar.dropna().values))
+                    self.minarrayctdi = np.append( self.minarrayctdi
+                                        , np.nanmin(CTDI_ar.dropna().values))
+                    self.maxarraydlp = np.append( self.maxarraydlp
+                                        , np.nanmax(DLP_ar.dropna().values))
+                    self.maxarrayctdi = np.append( self.maxarrayctdi
+                                        , np.nanmax(CTDI_ar.dropna().values))
                     dlpp=pd.to_numeric(DLP_ar, errors='coerce')
                     ctdii=pd.to_numeric(CTDI_ar, errors='coerce')
-                    self.medianarraydlp = np.append(self.medianarraydlp , np.nanmedian(dlpp))
-                    self.medianarrayctdi = np.append(self.medianarrayctdi , np.nanmedian(ctdii))
+                    self.medianarraydlp = np.append(self.medianarraydlp
+                                                        , np.nanmedian(dlpp))
+                    self.medianarrayctdi = np.append(self.medianarrayctdi
+                                                        , np.nanmedian(ctdii))
                     o = nn+" "+pp
                     self.descriptarray = np.append( self.descriptarray, o)
 
+        # If equipment was found for specific tests create a files
+        # and compute statistics such as the mean, median , and standard deviation
+        # etc. for each instituion and machine and output in an excel file
         strin= data["date"][1]
         strin = str(strin)
         print(strin)
@@ -104,7 +85,7 @@ class pediatric_general:
         string = strin.split("-")[0]+'.xlsx'
 
 
-    
+
         workbook = xlsxwriter.Workbook(string)
         worksheet = workbook.add_worksheet()
         bold = workbook.add_format({'bold': True})
@@ -124,25 +105,3 @@ class pediatric_general:
         worksheet.write_column(row, 5 , self.medianarrayctdi)
         worksheet.write_column(row, 6 , self.maxarrayctdi)
         workbook.close()
-    
-    
-    
-
-        
-            
-
-            #print(data_dev)
-            #print(data_dev["dlp"])
-        #try:
-           # print(nn+pp+"dlp")
-           # print(mini(data_dev["dlp"]))
-           # print( median(data_dev,"dlp"))
-           # print( maxi(data_dev["dlp"]))
-           # print(nn+pp+"icrp103")
-           # print(mini(data_dev["103_ICRP"]))
-           # print( median(data_dev,"103_ICRP"))
-           # print( maxi(data_dev["103_ICRP"]))
-        #except ValueError:
-          #  print('skip '+nn+pp)
-    #r.to_csv(o)
-    #print(r)
